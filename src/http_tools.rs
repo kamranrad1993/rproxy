94b6d3a68596b2d6060604f2c663c512fc13e0d7
kamran raei <Raie.k@entekhabelectronic.com>
2024-06-10 16:57:27 +0330

    next step

--- src/http_tools.rs
+++ src/http_tools.rs
@@ -1,8 +1,11 @@
 pub mod http_tools {
     use http::{Request, Response};
+    use rand::seq;
     use std::{
         fmt::Display,
-        io::{Read, Result, Write}, os::fd::AsRawFd,
+        io::{Read, Result, Write},
+        os::fd::AsRawFd,
+        iter::Iterator
     };
 
     pub fn write_response<T: Write, B: AsRef<[u8]>>(
@@ -28,30 +31,58 @@ pub mod http_tools {
         stream.write(buffer.as_ref())
     }
 
-    fn get_available_bytes<T: Read + AsRawFd>(stream: T) -> Result<usize>{
+    fn get_available_bytes<T: Read + AsRawFd>(stream: &mut T) -> Result<usize> {
         let mut available: usize = 0;
-            let result: i32 =
-                unsafe { libc::ioctl(stream.as_raw_fd(), libc::FIONREAD, &mut available) };
-            if result == -1 {
-                let errno = std::io::Error::last_os_error();
-                Err(errno)
-            } else {
-                Ok(available)
-            }
-    }
-
-    pub fn read_request<T: Read + AsRawFd, B: AsRef<[u8]>>(stream: T) -> Result<Request<B>> {
-        let size = get_available_bytes(stream)?;
-        let mut buffer = vec![0u8;size];
-
-        if let Err(e) = stream.read(&mut buffer){
-            return Err(e)
+        let result: i32 =
+            unsafe { libc::ioctl(stream.as_raw_fd(), libc::FIONREAD, &mut available) };
+        if result == -1 {
+            let errno = std::io::Error::last_os_error();
+            Err(errno)
+        } else {
+            Ok(available)
         }
-        
-        
     }
 
+    fn parse_header(data: &[u8]) -> Result<(&str, &str)> {
 
+    }
+
+    pub fn read_request<T: Read + AsRawFd, B: AsRef<[u8]>>(stream: &mut T) -> Result<Request<B>> {
+        let size = get_available_bytes(stream)?;
+        let mut buffer = vec![0u8; size];
+
+        if let Err(e) = stream.read(&mut buffer) {
+            return Err(e);
+        }
+
+        let sequence = vec![0usize; 0];
+        let buffer_iter = buffer.iter().peekable();
+        let separator_buf = vec![0u8; 0];
+        let mut has_body = false;
+        
+        for (index, &value) in buffer_iter.enumerate() {
+            match value {
+                b'\n' => {
+                    separator_buf.push(value);
+                    if separator_buf.len() > 2 {
+                        has_body = true;
+                    }
+                },
+                b'\r' => {
+                    if separator_buf.len() == 0 {
+                        sequence.push(index);
+                    }
+                    separator_buf.push(value);
+                },
+                _ =>{
+                    separator_buf.clear();
+                }
+            }
+        }
+        sequence.push(buffer.len());
+        let headers = vec![("", ""); 0];
+        
+    }
 
     // use std::collections::HashMap;
     // use std::fmt::Display;
